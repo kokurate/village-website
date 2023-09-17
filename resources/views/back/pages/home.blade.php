076a85c8148b523245175b68ac5c36c6cf3b878c
kokurate <fredriksanger@gmail.com>
2023-09-17 18:07:26 +0800

    Setting up admin dashboard and aparatur migration

--- resources/views/back/pages/home.blade.php
+++ resources/views/back/pages/home.blade.php
@@ -21,5 +21,128 @@
 @endsection
 @section('content')
 
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum officiis quos ab iure sit asperiores perferendis explicabo assumenda unde soluta nam inventore perspiciatis itaque enim nostrum maiores, molestias repellat ad! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime voluptate cupiditate reprehenderit velit. In optio nobis veritatis molestiae perspiciatis minima expedita, impedit tempora aspernatur amet quas dolorum possimus asperiores consectetur. Veritatis nisi unde enim et mollitia quis autem. Dolor quam quaerat asperiores mollitia enim distinctio iusto repellat consequatur sit commodi sunt iste, ut magni tempora? Delectus officiis reiciendis quis nobis sed dolorum porro excepturi omnis, eum recusandae, assumenda sit ex, quas ad ipsa? Repudiandae ipsam quis corrupti, fugiat quidem autem quas laboriosam facilis id commodi illum eaque! Nulla error laudantium perferendis vel. Id aut rem praesentium architecto, debitis vero ipsum nulla dicta temporibus odio enim eum unde quam delectus inventore harum explicabo atque non. Magnam reiciendis voluptatibus fuga, vero voluptates ipsa aliquam velit.
+<hr>
+<h2>Selamat Data <strong>{{ auth()->user()->name }}</strong></h2>
+
+<div class="page-body">
+  <div class="container-xl">
+    <div class="row row-cards">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Response times across regions in the last day</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Uptime incidents per day</h3>
+          </div>
+            <div class="card">
+              <div class="card-body">
+                <div id="carousel-captions" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item">
+                      <img class="d-block w-100" alt="" src="/back/static/photos/hero.jpg" ">
+                      <div class="carousel-caption-background d-none d-md-block"></div>
+                      <div class="carousel-caption d-none d-md-block">
+                        <h3>Slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" alt="" src="/back/static/photos/young-entrepreneur-working-from-a-modern-cafe-2.jpg">
+                      <div class="carousel-caption-background d-none d-md-block"></div>
+                      <div class="carousel-caption d-none d-md-block">
+                        <h3>Slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </div>
+                    </div>
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" alt="" src="/back/static/photos/woman-working-on-laptop-at-home-office.jpg">
+                      <div class="carousel-caption-background d-none d-md-block"></div>
+                      <div class="carousel-caption d-none d-md-block">
+                        <h3>Slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carousel-captions" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carousel-captions" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="card">
+
+          <div class="accordion" id="accordion-example">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="heading-1">
+                <button class="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="true">
+                 <h3>Aparatur Desa</h3>
+                </button>
+              </h2>
+              <div id="collapse-1" class="accordion-collapse collapse" data-bs-parent="#accordion-example">
+                <div class="accordion-body pt-0">
+                  
+                  
+
+                  <div class="card-table table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Time period</th>
+                          <th>Availability</th>
+                          <th>Downtime</th>
+                          <th>Incidents</th>
+                          <th>Longest incident</th>
+                          <th>Avg. incident</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Today</td>
+                          <td>98.9533%</td>
+                          <td>1 minute</td>
+                          <td>1</td>
+                          <td>1 minute</td>
+                          <td>1 minute</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {{-- <div class="accordion-item">
+              <h2 class="accordion-header" id="heading-2">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="false">
+                  Accordion Item #2
+                </button>
+              </h2>
+              <div id="collapse-2" class="accordion-collapse collapse" data-bs-parent="#accordion-example">
+                <div class="accordion-body pt-0">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div> --}}
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 @endsection
